f032110a0d8bb69501bef8c5f771cc4c48cc4ee6
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-19 12:17:02 -0400

    #7: change image references images/[image].png -> img/[image].png

--- js/recorder_attributes.js
+++ js/recorder_attributes.js
@@ -8,7 +8,7 @@
     
       var appWidth = 24;
       var appHeight = 24;
-      var flashvars = {'event_handler': 'microphone_recorder_events', 'upload_image': 'images/upload.png'};
+      var flashvars = {'event_handler': 'microphone_recorder_events', 'upload_image': 'img/upload.png'};
       var params = {};
       var attributes = {'id': "recorderApp", 'name':  "recorderApp"};
       swfobject.embedSWF("recorder.swf", "flashcontent", appWidth, appHeight, "11.0.0", "", flashvars, params, attributes);
