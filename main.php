2b33e026ea6240bfef55b1879aa15673abc5446e
Jeffrey <jeffrey@Chris-McFarlands-MacBook-Pro-15.local>
2014-05-20 11:13:50 -0400

    #7: remove inline js in main.php

--- main.php
+++ main.php
@@ -25,36 +25,6 @@ echo $output;
     <script type='text/javascript' src='js/recorder_initialize.js'></script>
 
     <link rel='stylesheet' type='text/css' href='css/main.css'>
-
-
-  <script type="text/javascript">
-    $(function() {
-      var gain = $('#gain')[0];
-      var silenceLevel = $('#silenceLevel')[0];
-      for(var i=0; i<=100; i++) {
-        gain.options[gain.options.length] = new Option(100-i);
-        silenceLevel.options[silenceLevel.options.length] = new Option(i);
-      }
-    
-      var appWidth = 24;
-      var appHeight = 24;
-      var flashvars = {'event_handler': 'microphone_recorder_events', 'upload_image': 'images/upload.png'};
-      var params = {};
-      var attributes = {'id': "recorderApp", 'name':  "recorderApp"};
-      swfobject.embedSWF("recorder.swf", "flashcontent", appWidth, appHeight, "11.0.0", "", flashvars, params, attributes);
-    });
-
-    function configureMicrophone() {
-      if(! FWRecorder.isReady) {
-        return;
-      }
-
-      FWRecorder.configure($('#rate').val(), $('#gain').val(), $('#silenceLevel').val(), $('#silenceTimeout').val());
-      FWRecorder.setUseEchoSuppression($('#useEchoSuppression').is(":checked"));
-      FWRecorder.setLoopBack($('#loopBack').is(":checked"));
-    }
-  </script>
-
   </head>
 
   <body>
